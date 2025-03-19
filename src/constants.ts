968b2cce116c66663acc0634bb0031e814ef7d10
IsmoilQaxramonov <frontqaxramonov@gmail.com>
2025-03-19 07:04:36 +0500

    added state

---
+++ src/constants.ts
@@ -0,0 +1,46 @@
+export const state = {
+    movies: [
+        {            
+            id: 1,
+            title: "Airplane",
+            ganre: "Comedy",
+            stock: 5,
+            rate: 4.5,
+        },
+        {            
+            id: 2,
+            title: "Die Hard",
+            ganre: "Action",
+            stock: 5.5,
+            rate: 3.5,
+        },
+        {            
+            id: 3,
+            title: "Get Out",
+            ganre: "Thriller",
+            stock: 4,
+            rate: 2.5,
+        },
+        {
+            id: 4,
+            title: "Gone Girl",
+            ganre: "Thriller",
+            stock: 8,
+            rate: 6.5,
+        },
+        {            
+            id: 5,
+            title: "Inception",
+            ganre: "Sci-Fi",
+            stock: 8,
+            rate: 7.5,
+        },
+        {
+            id: 6,
+            title: "Interstellar",
+            ganre: "Sci-Fi",
+            stock: 10,
+            rate: 10,
+        },
+    ]    
+}
